e9b2c6a3f6f6abcedaf4a68ce83bbff8fce7a352
riccardo <go_riky@hotmail.it>
2020-08-23 12:08:20 +0200

    Added Pepper location on map and direction for each market.
    Implemented:
    - Added images of arrow directions, pepper face and when clicking on one of the markets, an alert box appears on the screen asking for confimation. NOTE: this of course will be used either for normal usage of the app, or when the user is deaf.
    
    To implement:
    - The paths of arrows are all visible, hence they need to be set as initially invisible and then, depending on the selected market, show only the useuful ones.

--- app/src/main/java/com/example/myapplication/Indications.java
+++ app/src/main/java/com/example/myapplication/Indications.java
@@ -1,11 +1,10 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
-import android.widget.FrameLayout;
 
 import com.aldebaran.qi.sdk.QiContext;
 import com.aldebaran.qi.sdk.QiSDK;
@@ -19,6 +18,10 @@ public class Indications  extends RobotActivity implements RobotLifecycleCallbac
     private static final String TAG = "Disability";
     private QiContext qiContext;
 
+    public AlertDialog.Builder builder;
+    public AlertDialog confirmLocation;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +30,8 @@ public class Indications  extends RobotActivity implements RobotLifecycleCallbac
         setSpeechBarDisplayPosition(SpeechBarDisplayPosition.TOP);
         setContentView(R.layout.activity_indications);
         QiSDK.register(this, this);
-
+        //Build the AlertBox here, otherwise "context" is null
+        builder = new AlertDialog.Builder(this);
     }
 
     @Override
@@ -57,31 +61,186 @@ public class Indications  extends RobotActivity implements RobotLifecycleCallbac
 
         marketA.setOnClickListener(v->{
             Log.i(TAG, "Market A selected");
+            //Set title and message that should appear in the dialog box
+            builder.setTitle("Conferma");
+            builder.setMessage("Vuoi andare al negozio A?");
+            builder.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // TODO
+                    //Show arrows that goes from pepper to market A
+
+                    //Close the alert box
+                    dialog.dismiss();
+                }
+            });
+
+            builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //Simply close the alert box
+                    dialog.dismiss();
+                }
+            });
+            confirmLocation = builder.create();
+            confirmLocation.show();
         });
 
         marketB.setOnClickListener(v->{
             Log.i(TAG, "Market B selected");
+            builder.setTitle("Conferma");
+            builder.setMessage("Vuoi andare al negozio B?");
+            builder.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+
+                public void onClick(DialogInterface dialog, int which) {
+                    // TODO
+                    //Show arrows that goes from pepper to market B
+
+                    //Close the alert box
+                    dialog.dismiss();
+                }
+            });
+
+            builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            confirmLocation = builder.create();
+            confirmLocation.show();
         });
 
         marketC.setOnClickListener(v->{
             Log.i(TAG, "Market C selected");
+            builder.setTitle("Conferma");
+            builder.setMessage("Vuoi andare al negozio C?");
+            builder.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+
+                public void onClick(DialogInterface dialog, int which) {
+                    // TODO
+                    //Show arrows that goes from pepper to market C
+
+                    //Close the alert box
+                    dialog.dismiss();
+                }
+            });
+
+            builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            confirmLocation = builder.create();
+            confirmLocation.show();
         });
 
         marketD.setOnClickListener(v->{
             Log.i(TAG, "Market D selected");
+            builder.setTitle("Conferma");
+            builder.setMessage("Vuoi andare al negozio D?");
+            builder.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+
+                public void onClick(DialogInterface dialog, int which) {
+                    // TODO
+                    //Show arrows that goes from pepper to market D
+
+                    //Close the alert box
+                    dialog.dismiss();
+                }
+            });
+
+            builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            confirmLocation = builder.create();
+            confirmLocation.show();
         });
 
         marketE.setOnClickListener(v->{
 
             Log.i(TAG, "Market E selected");
+            builder.setTitle("Conferma");
+            builder.setMessage("Vuoi andare al negozio E?");
+            builder.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+
+                public void onClick(DialogInterface dialog, int which) {
+                    // TODO
+                    //Show arrows that goes from pepper to market E
+
+                    //Close the alert box
+                    dialog.dismiss();
+                }
+            });
+
+            builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            confirmLocation = builder.create();
+            confirmLocation.show();
         });
 
         marketF.setOnClickListener(v->{
             Log.i(TAG, "Market F selected");
+            builder.setTitle("Conferma");
+            builder.setMessage("Vuoi andare al negozio F?");
+            builder.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+
+                public void onClick(DialogInterface dialog, int which) {
+                    // TODO
+                    //Show arrows that goes from pepper to market F
+
+                    //Close the alert box
+                    dialog.dismiss();
+                }
+            });
+
+            builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            confirmLocation = builder.create();
+            confirmLocation.show();
         });
 
         marketG.setOnClickListener(v->{
             Log.i(TAG, "Market G selected");
+            builder.setTitle("Conferma");
+            builder.setMessage("Vuoi andare al negozio G?");
+            builder.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+
+                public void onClick(DialogInterface dialog, int which) {
+                    // TODO
+                    //Show arrows that goes from pepper to market G
+
+                    //Close the alert box
+                    dialog.dismiss();
+                }
+            });
+
+            builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            confirmLocation = builder.create();
+            confirmLocation.show();
         });
 
 
